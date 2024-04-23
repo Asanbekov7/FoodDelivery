edce2ef82e414850d5adf22582099b4290a88fa2
IronFed <fedotevseevfreelance@gmail.com>
2024-04-23 23:01:31 +0600

    Created base project, removed storyboard references

---
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Темирлан Асанбеков on 23/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
