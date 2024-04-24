ab8a58304e70804e58d8d12a9368b8455bba978d
IronFed <fedotevseevfreelance@gmail.com>
2024-04-24 14:41:49 +0600

    Added new base colors

---
+++ FoodDelivery/FoodDelivery/Resources/Appcolors.swift
@@ -0,0 +1,16 @@
+//
+//  Appcolors.swift
+//  FoodDelivery
+//
+//  Created by Темирлан Асанбеков on 24/4/24.
+//
+
+import UIKit
+
+enum Appcolors {
+    static let accentTomato = UIColor().hex(0xC8393D)
+    static let grey = UIColor().hex(0xECF0F1)
+    static let black = UIColor().hex(0x000000)
+    static let background = UIColor().hex(0xE5E5E5)
+    
+}
