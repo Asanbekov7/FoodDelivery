ff7c2407c27a162913a9e56aae65fce945e33418
IronFed <fedotevseevfreelance@gmail.com>
2024-04-24 14:23:31 +0600

    Added new fonts

---
+++ FoodDelivery/FoodDelivery/Resources/Fonts/Extensions/UIFont+Extension.swift
@@ -0,0 +1,104 @@
+//
+//  UIFont+Extension.swift
+//  FoodDelivery
+//
+//  Created by Темирлан Асанбеков on 24/4/24.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    enum Roboto {
+        enum Black {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.black, size: size)!
+            }
+        }
+        
+        enum BlackItalic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.blackItalic, size: size)!
+            }
+        }
+        
+        enum Bold {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.bold, size: size)!
+            }
+        }
+        
+        enum BoldItalic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.boldItalic, size: size)!
+            }
+        }
+        
+        enum Italic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.italic, size: size)!
+            }
+        }
+        
+        enum Light {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.light, size: size)!
+            }
+        }
+        
+        enum LightItalic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.lightItalic, size: size)!
+            }
+        }
+        
+        enum Medium {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.medium, size: size)!
+            }
+        }
+        
+        enum MediumItalic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.mediumItalic, size: size)!
+            }
+        }
+        
+        enum Regular {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.regular, size: size)!
+            }
+        }
+        
+        enum Thin {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.thin, size: size)!
+            }
+        }
+        
+        enum ThinItalic {
+            static func size(of size: CGFloat) -> UIFont {
+                return UIFont(name: Constants.Roboto.thinItalic, size: size)!
+            }
+        }
+    }
+}
+
+private extension UIFont {
+    enum Constants {
+        enum Roboto {
+            static let black = "Roboto-Black"
+            static let blackItalic = "Roboto-BlackItalic"
+            static let bold = "Roboto-Bold"
+            static let boldItalic = "Roboto-BoldItalic"
+            static let italic = "Roboto-Italic"
+            static let light = "Roboto-Light"
+            static let lightItalic = "Roboto-LightItalic"
+            static let medium = "Roboto-Medium"
+            static let mediumItalic = "Roboto-MediumItalic"
+            static let regular = "Roboto-Regular"
+            static let thin = "Roboto-Thin"
+            static let thinItalic = "Roboto-ThinItalic"
+        }
+    }
+}
--- FoodDelivery/FoodDelivery/ViewController.swift
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var lab: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.font = .Roboto.BoldItalic.size(of: 25)
+//        $0.textColor = .black
+        $0.text = "Hello world!"
+        return $0
+    }(UILabel())
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        setConstr()
     }
 
 
+    func setConstr() {
+        view.addSubview(lab)
+        NSLayoutConstraint.activate([
+            lab.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+            lab.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            lab.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+//            lab.heightAnchor.constraint(equalToConstant: 30)
+        
+        ])
+    }
 }
 
